66bc15aeae152c7d6c0e3ed47bc1939d5ba7f4cf
lodewyk <lodewyk@thinkninjas.co.za>
2022-08-10 07:01:20 +0200

    app initial setup

---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/models/product.dart';
+
+class ProductItem extends StatelessWidget {
+  final Product product;
+  const ProductItem({Key? key, required this.product}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridTile(
+      child: Image.network(
+        product.imageUrl,
+        fit: BoxFit.cover,
+      ),
+      footer: GridTileBar(
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.favorite),
+        ),
+        backgroundColor: Colors.black54,
+        title: Text(
+          product.title,
+          textAlign: TextAlign.center,
+        ),
+        trailing: IconButton(
+          icon: Icon(Icons.shopping_cart),
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
+}
