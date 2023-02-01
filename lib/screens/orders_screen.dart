5ef0dad9f121bea9488244b547c5fcd530a97ec8
lodewyk <lodewyk@thinkninjas.co.za>
2023-02-01 07:45:28 +0200

    Added a futurebuilder to orders screen

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -4,48 +4,39 @@ import 'package:shop_app/providers/order_provider.dart';
 import 'package:shop_app/widgets/app_drawer.dart';
 import 'package:shop_app/widgets/order_item_widget.dart';
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
   const OrdersScreen({super.key});
 
   @override
-  State<OrdersScreen> createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  bool _isLoading = false;
-
-  @override
-  void initState() {
-    super.initState();
-    Future.delayed(Duration.zero).then((_) async {
-      setState(() {
-        _isLoading = true;
-      });
-      await Provider.of<OrderProvider>(context, listen: false)
-          .fetchAndSetOrders();
-      setState(() {
-        _isLoading = false;
-      });
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<OrderProvider>(context);
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Your Orders'),
-      ),
-      drawer: const AppDrawer(),
-      body: _isLoading
-          ? const Center(
-              child: CircularProgressIndicator(),
-            )
-          : ListView.builder(
-              itemCount: orderData.orders.length,
-              itemBuilder: (context, index) =>
-                  OrderItemWidget(order: orderData.orders[index]),
-            ),
-    );
+        appBar: AppBar(
+          title: const Text('Your Orders'),
+        ),
+        drawer: const AppDrawer(),
+        body: FutureBuilder(
+          future: Provider.of<OrderProvider>(context, listen: false)
+              .fetchAndSetOrders(),
+          builder: (context, snapshot) {
+            if (snapshot.error != null) {
+              return const Center(
+                child: Text('An error occured'),
+              );
+            }
+            return snapshot.connectionState == ConnectionState.done
+                ? Consumer<OrderProvider>(
+                    builder: (context, orderData, child) {
+                      return ListView.builder(
+                        itemCount: orderData.orders.length,
+                        itemBuilder: (context, index) =>
+                            OrderItemWidget(order: orderData.orders[index]),
+                      );
+                    },
+                  )
+                : const Center(
+                    child: CircularProgressIndicator(),
+                  );
+          },
+        ));
   }
 }
